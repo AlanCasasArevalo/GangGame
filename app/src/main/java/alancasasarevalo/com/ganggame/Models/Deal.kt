6ed3339c86822becbe6cdd66565d92ea103c7946
Alan Casas <alancasasarevalo@gmail.com>
2018-02-10 19:30:49 +0100

    Data models added Some resources added Recycler view items modified to fix some appearance issues Some icon to menu added DataBinding added to gradle Generic DataBindingViewHolder and DataBindingRecyclerAdapter added

---
+++ app/src/main/java/alancasasarevalo/com/ganggame/Models/Deal.kt
@@ -0,0 +1,9 @@
+package alancasasarevalo.com.ganggame.Models
+
+data class Deal (   var title:String,
+                    var salePrice: Float,
+                    var normalPrice: Float,
+                    var metacriticScore: Int,
+                    var steamRating: Int,
+                    var thumb: String)
+
---
+++ app/src/main/java/alancasasarevalo/com/ganggame/Models/TopGame.kt
@@ -0,0 +1,10 @@
+package alancasasarevalo.com.ganggame.Models
+
+data class TopGame (var title:String,
+                    var owners: Int,
+                    var steamRating: Int,
+                    var publisher: String,
+                    var price: Float,
+                    var position: Int,
+                    var thumb: String)
+
